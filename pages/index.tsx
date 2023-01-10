9bad55b6647df963039dec63223594ecbbcd2125
prashant523580 <prashant.thapa62758@gmail.com>
2023-01-11 02:23:00 +0545

    replace ' to &apos;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import { useEffect, useRef } from 'react'
+// import { useEffect, useRef } from 'react'
 import HomeComponent from '../components/pages/Home'
 import ServiceComponents from '../components/pages/Service'
 import SkillComponent from '../components/pages/Skill'
-import ResumeComponent from '../components/pages/Resume'
+// import ResumeComponent from '../components/pages/Resume'
 // import PricingComponent from '../components/pages/Pricing'
 import VideoComponent from '../components/Video'
 import ContactComponent from '../components/pages/Contact'
@@ -14,17 +14,17 @@ import Testimonial from '../components/pages/Testimonial'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  let circleRef = useRef<any>();
+  // let circleRef = useRef<any>();
 
-  useEffect(() => {
-    if (typeof window != "undefined") {
+  // useEffect(() => {
+  //   if (typeof window != "undefined") {
 
-      window.onmousemove = ((e) => {
-        let left = e.offsetX;
-        let top = e.offsetY;
-      })
-    }
-  }, [circleRef.current])
+  //     window.onmousemove = ((e) => {
+  //       let left = e.offsetX;
+  //       let top = e.offsetY;
+  //     })
+  //   }
+  // }, [circleRef.current])
   return (
     <>
       {/* <div ref={circleRef} className={styles.circle}></div> */}
